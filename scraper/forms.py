8a23c15fd354c9a509e03e37ad0ce604f4bcef1f
Adebisi Ayomide <usiwoma.oghenovo@rosabon-finance.net>
2020-05-04 01:14:00 +0100

    alsaheem--finished code

--- scraper/forms.py
+++ scraper/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class Search(forms.Form):
     """Search definition."""
-    searchword = forms.CharField(label='Movie',widget=forms.TextInput(attrs={'class':'forms-control'}))
+    searchword = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Movie Title .....'}))
