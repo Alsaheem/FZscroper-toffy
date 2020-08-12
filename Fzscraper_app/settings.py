ca1aadbe8a02af8b74d58889cbda19874cd5cc2d
Adebisi Ayomide <usiwoma.oghenovo@rosabon-finance.net>
2020-08-12 01:50:51 +0100

    updated url

--- Fzscraper_app/settings.py
+++ Fzscraper_app/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
-    "https://pedantic-lewin-bdf7dc.netlify.app/"
+    "https://golddigger.netlify.app"
 ]
 
 MIDDLEWARE = [
